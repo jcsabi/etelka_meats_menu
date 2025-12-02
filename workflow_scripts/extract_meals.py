ec3c24f9efb7a50215bb2cbd5018dc138e53a1cd
Csaba Juhasz <csjuhasz@cloudera.com>
2025-12-02 16:00:48 +0100

    Fix PDF check

--- workflow_scripts/extract_meals.py
+++ workflow_scripts/extract_meals.py
@@ -63,8 +63,9 @@ One object per day. No extra text. Please remove everything from the meals match
 def check_meals_pdf_exists_for_month(url: str) -> bool:
     exists = False
     try:
-        resp = requests.head(url, timeout=10, allow_redirects=True)
-        print(f"Checking URL: {url}, response: {resp}")
+        print("Checking URL existence:", url)
+        resp = requests.get(url, timeout=10, allow_redirects=True, stream=True)
+        print(f"URL check response: {resp}, headers: {resp.headers}")
         exists = resp.status_code == 200 and "pdf" in resp.headers.get("Content-Type", "").lower()
     except requests.RequestException as e:
         print(f"Error checking URL, {url}: {e}")
